6549301c0e54db26da129d86e5121a51626912c9
subhaps <140229707+subhaps@users.noreply.github.com>
2023-08-04 21:13:09 +0530

    Update Currency.js

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,15 +1,16 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 const Currency = () => {
 
+    const [setCurrency] = useState('');
     return (
         <div className='alert alert-success'>
-            <select name="currency" id="currency" style="border-radius: 15px;
- background-color: red;">
-                <option value="dollor">$ Dollor</option>
-                <option value="pound">£ Pound</option>
-                <option value="euro">€ Euro</option>
-                <option value="rupee">₹ Rupee</option>
+            Currency:
+            <select name="currency" id="currency" style={{ borderRadius: "5px", backgroundColor: "lightgreen" }} onChange={(event) => setCurrency(event.target.value)}>
+                <option value="$">$ Dollor</option>
+                <option value="£">£ Pound</option>
+                <option value="€">€ Euro</option>
+                <option value="₹">₹ Rupee</option>
             </select>
         </div>
     );
